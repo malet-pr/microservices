360a3cdce69b6b6a1e02890e8c420e6f00105264
Nuria Malet Quintar <maletq.pr@gmail.com>
2024-10-13 23:29:13 -0300

    first rules-service commit

---
+++ rules/src/main/java/org/acme/rules/RulesApplication.java
@@ -0,0 +1,13 @@
+package org.acme.rules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesApplication.class, args);
+	}
+
+}
