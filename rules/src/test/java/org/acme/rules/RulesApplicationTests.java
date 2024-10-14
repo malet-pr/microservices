6dfa2d1494e678cf00222847628504a8d8560752
Nuria Malet Quintar <maletq.pr@gmail.com>
2024-10-13 23:29:13 -0300

    first rules-service commit

---
+++ rules/src/test/java/org/acme/rules/RulesApplicationTests.java
@@ -0,0 +1,15 @@
+package org.acme.rules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class RulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
