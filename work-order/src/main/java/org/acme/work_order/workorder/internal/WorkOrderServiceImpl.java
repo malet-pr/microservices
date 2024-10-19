3f85f25aa9676bd0139c64cda8801d1d73856ca4
Nuria Malet Quintar <maletq.pr@gmail.com>
2024-10-18 21:34:07 -0300

    adds logs and start update method

--- work-order/src/main/java/org/acme/work_order/workorder/internal/WorkOrderServiceImpl.java
+++ work-order/src/main/java/org/acme/work_order/workorder/internal/WorkOrderServiceImpl.java
@@ -1,10 +1,16 @@
 package org.acme.work_order.workorder.internal;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaUpdate;
+import jakarta.persistence.criteria.Root;
 import org.acme.work_order.grpc.*;
 import org.acme.work_order.grpc.TestBack;
 import org.acme.work_order.grpc.TestGo;
 import org.acme.work_order.workorder.WorkOrderDTO;
 import org.acme.work_order.workorder.WorkOrderService;
+import org.acme.work_order.workorderjob.internal.WorkOrderJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +30,8 @@ public class WorkOrderServiceImpl implements WorkOrderService {
     @Autowired
     private TestConnectionConsumer testConsumer;
 
+    @PersistenceContext
+    public EntityManager em;
 
     @Override
     public Boolean save(WorkOrderDTO dto) {
@@ -32,6 +40,7 @@ public class WorkOrderServiceImpl implements WorkOrderService {
         if(entity == null) {return false;}
         if(woDAO.existsByWoNumber(entity.getWoNumber())) {
             log.error("A work order with number {} already exists", entity.getWoNumber());
+            return false;
         }
         try {
             entity.getJobs().forEach(j -> j.setWorkOrder(entity));
@@ -42,6 +51,7 @@ public class WorkOrderServiceImpl implements WorkOrderService {
             // runTestConnection();
         }catch (Exception e) {
             log.error(e.getMessage());
+            return false;
         }
         return result;
     }
@@ -49,15 +59,33 @@ public class WorkOrderServiceImpl implements WorkOrderService {
     private void runRules(WorkOrderDTO dto) {
         try{
             WorkOrderDTO newDto = consumer.callRules(dto);
-            if(newDto != null) {
-                log.info("Successfully run rules for {}", newDto);
-                save(newDto);
-                log.info("Successfully saved work order {}", newDto);
-            } else {
-                log.error("Failed to save resulting dto {}", newDto);
+            log.info("Successfully run rules for {}", newDto);
+            boolean res = updateAfterRules(newDto);
+            if(res){
+                log.info("Successfully updated after applied rules for {}", newDto.getWoNumber());
             }
         }catch (Exception e) {
             log.error("Failed to run rules - " + e.getMessage());
+        }
+    }
+
+    private Boolean updateAfterRules(WorkOrderDTO dto) {
+        try{
+            WorkOrderDTO old = this.findByWoNumber(dto.getWoNumber());
+            if(old == null) {return false;}
+            String updateQuery = "UPDATE wo.wo_job SET quantity=:quantity,active_status=:activeStatus WHERE work_order_id=:id";
+            em.createNativeQuery(updateQuery);
+
+            // TODO:
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaUpdate<WorkOrderJob> update = cb.createCriteriaUpdate(WorkOrderJob.class);
+
+
+            return true;
+        } catch (Exception e){
+            log.error("Failed to update after applied rules for {}", dto.getWoNumber());
+            log.error("Cause: {}",e.getMessage());
+            return false;
         }
     }
 
@@ -77,4 +105,5 @@ public class WorkOrderServiceImpl implements WorkOrderService {
     public WorkOrderDTO findByWoNumber(String woNumber) {
         return woMapper.convertToDTO(woDAO.findByWoNumber(woNumber));
     }
+
 }
