4778f46b6a61180ff45ced5400fc6ff69df26e95
Nuria Malet Quintar <maletq.pr@gmail.com>
2024-10-18 21:34:07 -0300

    adds logs and start update method

--- work-order/src/main/java/org/acme/work_order/workorder/internal/WorkOrderDAO.java
+++ work-order/src/main/java/org/acme/work_order/workorder/internal/WorkOrderDAO.java
@@ -1,6 +1,7 @@
 package org.acme.work_order.workorder.internal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WorkOrderDAO extends JpaRepository<WorkOrder,Long> {
     WorkOrder findByWoNumber(String woNumber);
