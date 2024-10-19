88edd0c662c90fd6853e49ae55fb69c0b9edb6eb
Nuria Malet Quintar <maletq.pr@gmail.com>
2024-10-19 15:57:04 -0300

    fixes tests after adding schema to the queries

--- work-order/src/test/resources/sql/jobTypeTestData.sql
+++ work-order/src/test/resources/sql/jobTypeTestData.sql
@@ -1,11 +1,11 @@
-truncate table work_order, wo_job,job, job_type cascade;
+truncate table wo.work_order, wo.wo_job, wo.job, wo.job_type cascade;
 
-ALTER SEQUENCE job_type_seq RESTART WITH 1;
+ALTER SEQUENCE wo.job_type_seq RESTART WITH 1;
 
-insert into job_type (id,creation_date,active_status,client_type,code,name)
+insert into wo.job_type (id,creation_date,active_status,client_type,code,name)
 values
     (1,NOW(),'Y','consumer','type1','job type name 1'),
     (2,NOW(),'Y','corporate','type2','job type name 2'),
     (3,NOW(),'N','corporate','type3','job type name 3');
 
-ALTER SEQUENCE job_type_seq RESTART WITH 10;
+ALTER SEQUENCE wo.job_type_seq RESTART WITH 10;
